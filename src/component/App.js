728533fbb2bc81519bb53789fd4763de89b3792b
goddaehee <dedurox2@gmail.com>
2022-02-05 20:02:37 +0900

    미사용 컴토넌트 제거

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
@@ -22,3 +22,4 @@ const App = () => {
 }
 
 export default App;
+  
