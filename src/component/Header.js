c6e955764a9f3b120f726440f4cde69612b945f3
mengga <zonefors@gmail.com>
2022-04-23 11:41:25 +0900

    미어켓켓

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-function Header(props) {
-    return (
-        <>
-            <Link to="/">
-                <h1>헤더입니다.</h1>
-            </Link>
-        </>
-    );
+export default function Header({ score, bestScore }) {
+  return (
+    <header className="heading">
+      <h1 className="title">2048</h1>
+      <div className="scores-container">
+        <div className="score-container" style={{ marginRight: 5 }}>
+          {score}
+        </div>
+        <div className="best-container">{bestScore}</div>
+      </div>
+    </header>
+  );
 }
-
-export default Header;
